defe9f55e7e3ddfcb69fb382199f9fe9e35d05df
= <=>
2022-09-09 01:56:05 +0100

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include < stdio.h >
+/*
+*main prints size in memory
+*/
+int main(void)
+{
+puts("Size of a char: %zu byte(s)\n", sizeof(char));
+puts("Size of an int: %zu byte(s)\n", sizeof(int));
+puts("Size of a long int: %zu byte(s)\n", sizeof(long int));
+puts("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+puts("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
+}
