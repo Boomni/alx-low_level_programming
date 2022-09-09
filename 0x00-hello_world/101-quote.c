ac7f4c29b477672c55c5a78bff7d6e7ac2211fdd
= <=>
2022-09-09 03:33:07 +0100

    cf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1)
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return(1);
 }
