d7eff9601834e714d0e52e9c7516515383461a52
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-13 10:14:08 -0700

    d

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * _islower - Entry point after going through main
+ * _isalpha - Entry point after going through main
  * @c: the integer value it receives
  * Description: Make the alphabet 10 times
  * Return: 1 if true, 0 if false
