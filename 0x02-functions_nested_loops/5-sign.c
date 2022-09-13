ab14a0dc72411dfa69cd8eba70fd2097ec57eaca
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-13 11:47:22 -0700

    d

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+/* Author: Jonathan Boomni */
+
+#include "main.h"
+
+/**
+ * print_sign - Entry point after going through main
+ * @n: the integer value it receives
+ * Description: Prints the sign of a number
+ * Return: 1 if positive, 0 if zero and -1 if negative
+ */
+int print_sign(int n)
+{
+	int a;
+
+	if (a > 0)
+		print("+");
+			return (1);
+	else if (n = 0)
+		printf("0");
+			return (0);
+	else if (n < 0)
+		printf("-");
+			return (-1);
+}
