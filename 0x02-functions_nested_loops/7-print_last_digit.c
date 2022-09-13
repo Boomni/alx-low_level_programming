e86561b449c00415ae469067a990a8d7ba73cbf9
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2022-09-13 20:53:26 +0100

    Update 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+/* Author: Jonathan Boomni */
+
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point after going through main
+ * @n: the integer value it receives
+ * Description: Prints the last digit of an integer
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+	
+}
