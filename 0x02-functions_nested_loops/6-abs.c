52bfee6e4e4e0ea21fead8a59620a29060990eb0
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2022-09-13 20:42:28 +0100

    Update 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+/* Author: Jonathan Boomni */
+
+#include "main.h"
+
+/**
+ * _abs - Entry point after going through main
+ * @n: the integer value it receives
+ * Description: Prints the absolute value of an integer
+ * Return: int
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (0);
+	}
+}
