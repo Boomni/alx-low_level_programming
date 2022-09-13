d53bd27a94d316013dc1ebff8026644f106f8fba
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-13 14:41:48 -0700

    d

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,10 +13,9 @@ int print_sign(int n)
 	if (n > 0)
 		printf("+");
 			return (1);
-	else if (n = 0)
-		printf("0");
-			return (0);
 	else if (n < 0)
 		printf("-");
 			return (-1);
+	printf("0");
+	return (0);
 }
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# alx-low_level_programming
-This is my first repository on C programming
+Debugging
