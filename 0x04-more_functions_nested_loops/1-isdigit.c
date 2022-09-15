98ab6a80c5640d2032f46eacf3989c97a0b5227d
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-15 04:43:03 -0700

    s

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,12 +5,12 @@
 /**
  * _isdigit - Called from main
  * @c: Integer declared
- * Description: Detects digits 0 to 9
+ * Description: Detects digits 0 to 9 with ascii
  * Return: 1 for digit
  */
 int _isdigit(int c)
 {
-	if (c = 0 && c < 10)
+	if (c >= 48 && c <= 57)
 		return (1);
 	return (0);
 }
