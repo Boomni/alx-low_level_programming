b97f5612481b45b9e07fdcce9411608250dc0956
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-15 03:13:15 -0700

    q

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_to_98 - Print all natural numbers from n to 98
  * @n: int type number
@@ -32,3 +33,4 @@ void print_to_98(int n)
 	}
 	printf("\n");
 }
+
