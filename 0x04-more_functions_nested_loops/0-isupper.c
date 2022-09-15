0efdb981eb574bb132d21cc1a297c4d1c5163a1a
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-15 04:15:07 -0700

    f

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _isupper - Called from main
+ * @c: Integer declared
+ * Description: Detects uppercase
+ * Return: 1 for uppercase 0 for lowercase
+ */
+int _isupper(char c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,1 @@
+int _isupper(char c);
