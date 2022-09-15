3e5d559feb4f59a0491970e98d5c95023e7c47e6
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-15 04:38:59 -0700

    s

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _isdigit - Called from main
+ * @c: Integer declared
+ * Description: Detects digits 0 to 9
+ * Return: 1 for digit
+ */
+int _isdigit(int c)
+{
+	if (c = 0 && c < 10)
+		return (1);
+	return (0);
+}
+
