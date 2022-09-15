08db6e24f4445eac8744f7e7258ee2b8d5c1c206
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-15 05:02:41 -0700

    e

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - Called from main
+ * Description: prints numbers 0 to 9 followed by a comma
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		_putchar(x);
+	_putchar('\n');
+	return (0);
+}
+
