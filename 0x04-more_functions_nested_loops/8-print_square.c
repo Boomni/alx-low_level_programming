28e12de8ed7ec41f2ba8af0fd0b9b46261012a49
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-16 15:02:32 -0700

    fd

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,13 +10,21 @@
  */
 void print_square(int size)
 {
-	int a = 1;
-
-	while (size >= a)
+	if (size <= 0)
 	{
-		putchar('#');
-		a++;
+		putchar('\n');
 	}
-	putchar('\n');
-}
+	else
+	{
+		int i, j;
 
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
