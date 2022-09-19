339416326528d93936ac98e73ab12c664ed14dcd
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-19 14:34:39 -0700

    g

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - Entry point after main
+ * Description: Reverses a string
+ * Author: Jonathan Boomni
+ * @s: character string passed
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int x;
+	int len = strlen(s);
+
+	x >= len - 1;
+	x =0;
+	x--;
+}
+
