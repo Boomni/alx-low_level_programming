3c60a8026e5b7a0e23c7fa9a86dcf57c8fa7101b
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-19 12:13:34 -0700

    d

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Entry point after main
+ * Description: Prints length of a string
+ * Author: Jonathan Boomni
+ * @s: character string passed
+ * Return: String lenght
+ */
+int _strlen(char *s)
+{
+	int length = strlen(s);
+
+	return (length);
+}
+
