1822ca6c108cf0b285fee839c73de8daa870bac4
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-20 10:42:01 +0100

    fd

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,14 +9,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (src[i])
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
