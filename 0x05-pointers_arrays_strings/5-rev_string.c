5ec15c383ae7ce41bae12a6cfbb160922b3ff77a
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-20 08:53:22 +0100

    Update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,6 +12,14 @@
  */
 void rev_string(char *s)
 {
-	strrev(s);
-}
+	char *r_ptr = s;
+	while (*(r_ptr + 1) != '\0')
+		r_ptr++;
 
+	while (r_ptr > s)
+	{
+		char tmp = *r_ptr;
+		*r_ptr-- = *s;
+		*s++ = tmp;
+	}
+}
