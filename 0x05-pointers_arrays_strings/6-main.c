e1e5f8a145a48aee2b965fb152c883a0a284541b
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-20 09:17:46 +0100

    h

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void puts2(char *str);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "0123456789";
+    puts2(str);
+    return (0);
+}
