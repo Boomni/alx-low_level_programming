69a393c33a50ee97b8754225aa1642f7a0ebab34
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-20 10:07:46 +0100

    mm

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Entry point after main
+ * Description: Prints n elements of an array of integers, followed by a new line.
+ * Author: Jonathan Boomni
+ * @a: integer array passed
+ * @n: number of elements in array to be printed
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; n--, i++)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
