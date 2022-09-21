989e8fb9e4d745c1cdd4d9eb694a693592e56785
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-21 22:22:52 +0100

    k

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcat - concatenates a string
+ * @dest: string designation
+ * @src: string source
+ * Return: designation
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest,src);
+	return (dest);
+}
+
