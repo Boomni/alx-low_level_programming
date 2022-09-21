4bc9e692747f216b338cba582166d45a3478a90d
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-22 00:02:55 +0100

    hcg

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+	char s1[] = "Hello";
+	char s2[] = "World!";
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
-    return (0);
+	printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s2, s1));
+	printf("%d\n", _strcmp(s1, s1));
+	return (0);
 }
 
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,19 +3,27 @@
 #include <stdio.h>
 
 /**
- * _strncpy - concatenates a string
- * @s1: string to compair
- * @s2: string to compair
- * Return: 
+ * _strcmp - compairs two string
+ * Description: compairs two strings
+ * @s1: string passed
+ * @s2: string passed
+ * Return: differenc btw ascii values
  */
 int _strcmp(char *s1, char *s2)
 {
+/*
+ * The condition simply says keep looping until the end of
+ * the string is not reached and corresponding characters are same.
+ */
 	while ((*s1 && *s2) && (*s1 == *s2))
 	{
 		s1++;
 		s2++;
 	}
-
+/*
+ * The expression *strg1 - *strg2 evaluates the difference of the ASCII
+ * value of characters.
+*/
 	return (*s1 - *s2);
 }
 
