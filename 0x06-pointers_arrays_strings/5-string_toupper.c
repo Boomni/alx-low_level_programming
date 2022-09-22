73afe7c1620e3ce65e8d3246e00d11c659e8142f
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-22 01:00:00 +0100

    dsz

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * string_toupper - called from main
+ * @str: string passed
+ * Description: changes string to upper
+ * Return: char
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for(i=0; str[i]!='\0'; i++)
+	{
+		str[i] = toupper(str[i]);
+	}
+	return str;
+}
+
