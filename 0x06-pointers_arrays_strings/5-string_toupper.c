66655139de891bb098605b1d61e184926188c51e
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-22 01:16:42 +0100

    d

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,10 +12,10 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for(i=0; str[i]!='\0'; i++)
+	for (i=0; str[i] != '\0'; i++)
 	{
 		str[i] = toupper(str[i]);
 	}
-	return str;
+	return (str);
 }
 
