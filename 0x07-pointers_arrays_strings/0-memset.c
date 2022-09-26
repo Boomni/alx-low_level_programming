7a4f99c25480362df46ce47727e77113599e6b82
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-26 22:06:01 +0100

    Added READme.md, main.c and 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _memset - entry point from main
+ * Description: fills memory with a constant byte
+ * @s: Char passed
+ * @b: Char passed
+ * @n: An unsigned integer passed
+ * Return: A character value
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+
+	return (s);
+}
+
