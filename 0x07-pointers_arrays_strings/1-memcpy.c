8ff2c1e1d7c68834af21b292be4cdb590a6c1c9f
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-09-26 22:24:22 +0100

    dndsedc

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _memcpy - called from main
+ * Description: copies memory area
+ * @dest: designation character passed
+ * @src: source character passed
+ * @n: An unsigned integer passed
+ * Return: A character value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+
+	return (dest);
+}
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,6 @@
 
 
 char *_memset(char *s, char b, unsigned int n);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
