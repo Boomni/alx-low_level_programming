1e7aa2e69021bdeadb1f18aba5ed168eb72bf4c9
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-01 22:41:37 +0100

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - multiplies two numbers followed by a new line
@@ -9,8 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int index, result;
-	result = atoi(argv[1]) * atoi(argv[2]);
+	int  result = atoi(argv[1]) * atoi(argv[2]);
 
 	if (argc != 3)
 	{
@@ -18,8 +18,7 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	else
-		for (index = 1; index < argc; index++)
-			printf("%i\n", result);
+		printf("%i\n", result);
 	return (0);
 }
 
