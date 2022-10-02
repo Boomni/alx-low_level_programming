c9ae84e1d2391146d02c36919e34a0684b1c04d8
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-03 00:12:47 +0100

    sjas

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,11 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0)
 	{
-		if (month > 2 && day >= 60)
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31,     year);
+		}
+		else if (month > 2 && day >= 60)
 		{
 			day++;
 			printf("Day of the year: %d\n", day);
@@ -22,16 +26,9 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 	else
-	{
-		if (month == 2 && day == 60)
-		{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
-	}
 }
 
