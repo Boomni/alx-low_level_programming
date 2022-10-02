e0dae01164f8102448afb937b5a4fa981fde6487
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-02 02:19:01 +0100

    xds

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,24 +11,24 @@
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
-	
+
 	(void) argv;
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) > 0 || atoi(argv[i]) < 2147483647)
 			{
-				if (atoi(argv[i]) > 0 || atoi(argv[i]) < 2147483647)
-				{
-					sum += atoi(argv[i]);
-					printf("%i\n", sum);
-				}
-				else
-				{
-					printf("Error\n");
-					return (1);
-				}
+				sum += atoi(argv[i]);
+				printf("%i\n", sum);
 			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 	}
 	else
 		printf("0\n");
