f17486b077427074a23fe55c45d70d2c5de9b222
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-02 03:39:01 +0100

    sds

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _isdigit - Called from main
+ * @c: Integer declared
+ * Description: Detects digits 0 to 9 with ascii
+ * Return: 1 for digit
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+
