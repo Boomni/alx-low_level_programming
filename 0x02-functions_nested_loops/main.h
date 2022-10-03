d0300313c9541430d6f0af9de9ffd99827490c1f
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2022-10-03 23:52:38 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char x);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,3 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
+#endif /* MAIN_H */
