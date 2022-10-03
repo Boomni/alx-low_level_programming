034c4db6676ebd66c71dd32c9aad8d9308929708
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-03 01:46:56 +0100

    yes

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,9 +13,9 @@ int main(void)
 	int n;
 	int sum = 0;
 
-	for (n = 2; n < 1024; n++)
+	for (n = 0; n < 1024; n++)
 	{
-		if ((n % 3) == 0 && (n % 5) == 0)
+		if ((n % 3) == 0 || (n % 5) == 0)
 			sum += n;
 	}
 	printf("%i\n", sum);
