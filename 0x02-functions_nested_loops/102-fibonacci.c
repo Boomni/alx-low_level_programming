e010a63f9d9eebff8d4dc87481dafa09de198dd4
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-05 03:18:51 +0100

    sss

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,20 +9,21 @@
 int main(void)
 {
 	int i;
-	int n = 50;
 	unsigned long n1 = 0, n2 = 1, n3;
 
 	for (i = 0; i < 50; i++)
 	{
 		n3 = n1 + n2;
-		printf("%li", n3);
-
 		n1 = n2;
 		n2 = n3;
-		if (n != 48)
-			printf(", ");
+		if (i == 49)
+		{
+			printf("%li\n, ", n3);
+		}
 		else
-			printf("\n");
+		{
+			printf("%li, ", n3);
+		}
 	}
 	return (0);
 }
