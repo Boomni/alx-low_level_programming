489ec0dd31067d32206cbd65358447ead6a452f6
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-06 22:07:13 +0100

    sdvh

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -41,7 +41,7 @@ int main(void)
 	simple_print_buffer(buffer, 98);
 	_memset(buffer, 0x01, 95);
 	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);    
+	simple_print_buffer(buffer, 98);
 	return (0);
 }
 
