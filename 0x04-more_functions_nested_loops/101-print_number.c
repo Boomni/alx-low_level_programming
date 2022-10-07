45f4ab049ed76eac3426d31ffbae8b26a3b71600
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2022-10-07 22:20:12 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,12 +13,12 @@ unsigned int num = n;
 
 if (n < 0)
 {
-putchar('-');
+_putchar('-');
 num = -num;
 }
 
 if ((num / 10) > 0)
 print_number(num / 10);
 
-putchar((num % 10) + '0');
+_putchar((num % 10) + '0');
 }
