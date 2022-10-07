affa34a824c7f1aa59a258c5524b29a2b70e5f94
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2022-10-07 22:21:32 +0100

    Update 101-main.c

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -9,14 +9,14 @@
 int main(void)
 {
 	print_number(98);
-	putchar('\n');
+	_putchar('\n');
 	print_number(402);
-	putchar('\n');
+	_putchar('\n');
 	print_number(1024);
-	putchar('\n');
+	_putchar('\n');
 	print_number(0);
-	putchar('\n');
+	_putchar('\n');
 	print_number(-98);
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
