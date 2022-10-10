d7569764c3f14589804df3a568cddd141c5a8594
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2022-10-10 20:12:34 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,9 +13,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		d->name = name;
+		d->name = *name;
 		d->age = age;
-		d->owner = owner;
+		d->owner = *owner;
 	}
 }
 
