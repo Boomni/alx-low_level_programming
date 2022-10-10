861075710c4899f62e52e281ea28636d5252d810
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2022-10-10 12:53:01 +0100

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
-#ifndef ABS
+#ifndef 3-FUNCTION_LIKE_MACRO_H
+#define 3-FUNCTION_LIKE_MACRO_H
+
 #define ABS(x) ((x) < (0) ? (x) * (-1) : (x))
-#endif /* ABS(x) */
+
+#endif /* 3-FUNCTION_LIKE_MACRO_H */
