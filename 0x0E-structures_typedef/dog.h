c3b7c0f183080a7415f01bace58f46e64ecf1061
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2022-10-11 03:18:46 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,10 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * dog_t - A new type describing a dog.
+ * @d: struct passed
+ */
 typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
