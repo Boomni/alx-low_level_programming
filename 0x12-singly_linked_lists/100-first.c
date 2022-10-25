cf88a580ecb7a58f54aceda5b4e59701113f873a
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-26 00:33:43 +0100

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -14,15 +14,3 @@ void myStartupFun(void)
 	printf("I bore my house upon my back!\n");
 }
 
-/**
- * main - entry point
- *
- * Return: 0
- */
-int main(void)
-{
-	printf("(A tortoise, having pretty ");
-	printf("good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
-}
-
