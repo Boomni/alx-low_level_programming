569992e4038449a7f1177f78675e6e57a320b4bb
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-25 22:15:42 +0100

    sfs

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,12 +15,12 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-typedef long unsigned int size_t;
+typedef unsigned long int size_t;
 size_t print_list(const list_t *h);
 
 #endif /* MAIN_H */
