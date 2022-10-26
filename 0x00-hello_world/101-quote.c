7daf152a1f55454fb54de3e5cb43567e3f78a163
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-26 15:04:03 +0100

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return: 1
  */
 int main(void)
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+	printf("and that piece of art is useful\" ");
+	printf("- Dora Korpar, 2015-10-19\n");
+	return (1);
 }
