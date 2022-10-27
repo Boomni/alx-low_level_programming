bc51eb1791db0d8e8f3c75501c05347e12cc620f
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-27 14:21:43 +0100

    solved

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,6 +20,8 @@ unsigned int binary_to_uint(const char *b)
 	/* from the element before the null byte to the first element */
 	for (i = (len - 1); i >= 0; i--)
 	{
+		if ((b[i] != '0') && (b[i] != '1'))
+			return (0);
 		if (b[i] == '1')
 		{
 			dec += val;
