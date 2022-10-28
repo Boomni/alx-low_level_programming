bda595e4de0f5cd2a2e770fabf141721d7035d0b
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-28 01:14:20 +0100

    solved

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,11 +13,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 64)
 		return (-1);
-	else
-	{
-		(*n) |= (1 << index);
-		if ((*n) & (1 << index))
-			return (1);
-	}
+	(*n) |= (1 << index);
+	if ((*n) & (1 << index))
+		return (1);
 	return (0);
 }
