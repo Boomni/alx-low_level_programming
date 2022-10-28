e72189cb441535907a2c38b6896ad34f18cd4bf1
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-28 01:11:57 +0100

    good

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,9 +14,10 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > 64)
 		return (-1);
 	else
+	{
 		(*n) |= (1 << index);
 		if ((*n) & (1 << index))
 			return (1);
-
+	}
 	return (0);
 }
