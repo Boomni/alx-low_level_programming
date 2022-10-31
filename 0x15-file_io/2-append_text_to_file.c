e1caf5097b64f169061d9a31ab1995fc8a3ac7e8
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-31 18:36:34 +0100

    done

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to file
+ *
+ * @filename: is the file to read
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success and -1 on failure
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, write_status, words = 0;
+
+	/* check if filename is present */
+	if (filename == NULL)
+		return (0);
+
+	/* open file and redirect the return value of file into fd */
+	file = open(filename, O_CREAT | O_WRONLY | O_APPEND, S_IRUSR | S_IWUSR);
+	/* check if open was successful */
+	if (file == -1)
+		return (0);
+
+	if (text_content) /*write content to file if its not NULL*/
+	{
+		while (text_content[words] != '\0') /*find number of words*/
+			words++;
+
+		/*write to file*/
+		write_status = write(file, text_content, words);
+		if (write_status == -1) /*check if write was a succesful */
+			return (-1);
+	}
+
+	close(file);
+
+	return (0);
+}
+
