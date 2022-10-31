e52a582565a69136be131b57079f703e745db6f0
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-31 21:36:07 +0100

    done

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+#define S_E STDERR_FILENO
+
+/**
+ * main - a program that copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int input_fd, output_fd, rstatus, ostatus, wstatus;
+	char buf[1024];
+
+	/* if argument count is not 3 return an error */
+	if (argc != 3)
+		dprintf(S_E, "Usage: cp file_from file_to\n"), exit(97);
+
+	/* set input file descripor */
+	input_fd = open(argv[1], O_RDONLY);
+	if (input_fd == -1)
+	{
+		dprintf(S_E, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	/* set output file descriptor v*/
+	output_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (output_fd == -1)
+		dprintf(S_E, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	/* read file */
+	rstatus = read(input_fd, buf, 1024);
+	if (rstatus == -1)
+		dprintf(S_E, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	if (rstatus > 0)
+	{
+		wstatus = write(output_fd, buf, (ssize_t) rstatus);
+		if (wstatus == -1)
+			dprintf(S_E, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+
+	rstatus = close(input_fd);
+	if (rstatus == -1)
+		dprintf(S_E, "Error: Can't close fd %d\n", input_fd), exit(100);
+	ostatus = close(output_fd);
+	if (ostatus == -1)
+		dprintf(S_E, "Error: Can't close fd %d\n", output_fd), exit(100);
+	return (0);
+}
+
