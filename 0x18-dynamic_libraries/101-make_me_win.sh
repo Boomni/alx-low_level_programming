16c9729fb9860e87541b15996bd440c0f4cc53a8
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-12 22:40:20 +0100

    oh no

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/boomni/alx-low_level_programming/master/0x18-dynamic_libraries/rand_n.so
+wget -P /tmp/ https://github.com/Boomni/alx-low_level_programming/blob/main/0x18-dynamic_libraries/rand_n.so
 export LD_PRELOAD=/tmp/rand_n.so
