6f0e95b9f46acd4d8cb8640ebb5065dff2830002
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-01-06 23:20:49 +0100

    completed

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,16 +8,11 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash;
+	unsigned long int hash = 5381;
 	int c;
 
-	hash = 5381;
-
-	/* Iterate over each character of the string */
 	while ((c = *str++))
-	{
-		/* Update hash based on current character and previous value of hash */
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
+
 	return (hash);
 }
