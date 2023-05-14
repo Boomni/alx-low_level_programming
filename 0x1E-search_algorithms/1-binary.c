668453a75bb24dd6f64e98d05bc661e0996d5e9e
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-14 19:55:48 +0100

    Implemented binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -6,22 +6,40 @@
  * Description: uses the Binary search algorithm
  *
  * @array: the array to search
- * @size: The size of the array
+ * @size: The number of elements in the array
  * @value: the value that is being searched for
  *
  * Return: First index where value is located or -1 if not found
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid;
 	size_t i;
 
 	if (array == NULL)
 		return (-1);
-	for (i = 0; i < size; i++)
+
+	while (left <= right)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		printf("Searching in array:");
+		for (i = left; i <= right; i++)
+		{
+			printf(" %d", array[i]);
+			if (i < right)
+				printf(",");
+		}
+		printf("\n");
+
+		mid = ((left + right) / 2);
+
+		if (array[mid] == value)
+			return (mid);
+		else if (value < array[mid])
+			right = mid - 1;
+		else
+			left = mid + 1;
 	}
 	return (-1);
 }
